9859778a0fb084cca5ff75c44ace2f2b86dc4ebc
LiamSchippers <31887438+LiamSchippers@users.noreply.github.com>
2019-04-01 15:54:20 +0200

    Added ServerMessage class can now print direct messages and broadcast message correctly

---
+++ src/ServerMessage.java
@@ -0,0 +1,59 @@
+public class ServerMessage {
+
+    private String line;
+
+    ServerMessage(String line) {
+        this.line = line;
+    }
+
+    ServerMessage.MessageType getMessageType() {
+        ServerMessage.MessageType result = ServerMessage.MessageType.UNKNOWN;
+
+        try {
+            if (line != null && line.length() > 0) {
+                String[] splits = line.split("\\s+");
+                String lineTypePart = splits[0];
+                if (lineTypePart.startsWith("-") || lineTypePart.startsWith("+")) {
+                    lineTypePart = lineTypePart.substring(1);
+                }
+
+                result = ServerMessage.MessageType.valueOf(lineTypePart);
+            }
+        } catch (IllegalArgumentException var4) {
+            System.out.println("[ERROR] Unknown command");
+        }
+
+        return result;
+    }
+
+    String getPayload() {
+        if (getMessageType().equals(ServerMessage.MessageType.UNKNOWN)) {
+            return line;
+        } else if (line != null && line.length() >= getMessageType().name().length() + 1) {
+            int offset = 0;
+            if (getMessageType().equals(ServerMessage.MessageType.OK) ||
+                    getMessageType().equals(ServerMessage.MessageType.ERR)) {
+                offset = 1;
+            }
+
+            return line.substring(getMessageType().name().length() + 1 + offset);
+        } else {
+            return "";
+        }
+    }
+
+    public String toString() {
+        return line;
+    }
+
+    public enum MessageType {
+        HELO, BCST, PING, QUIT,
+        CLTLIST, PM, GRP_CREATE, GRPLIST, GRP_JOIN, GRP_SEND, GRP_LEAVE, GRP_KICK,
+        OK, ERR, UNKNOWN;
+
+        MessageType(){
+
+        }
+
+    }
+}
